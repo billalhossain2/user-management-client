ab259dfd16578790b06a58ae09c071ee66f30671
billal-webdev <billal-webdev@gmail.com>
2023-09-08 17:21:13 +0600

    code organised

--- src/pages/AddUser.jsx
+++ src/pages/AddUser.jsx
@@ -1,51 +1,114 @@
-import React from 'react'
-import Header from '../components/Header'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 const AddUser = () => {
   return (
     <div>
-        <Header></Header>
-        <Link to="/">
-        <p><i class="fa-solid fa-arrow-left-long me-2 my-3"></i><span>All Users</span></p>
-        </Link>
-        <div className='bg-[#ece8e8] py-5 flex flex-col items-center'>
-            <div className='text-center my-5'>
-            <h3 className='text-2xl font-semibold mb-2'>New User</h3>
-            <p className='text-[16px] text-[#615d5d]'>Use the below form to create a new user</p>
-            </div>
-            <form className='w-[60%] mx-atuo p-3 space-y-4'>
-                <div>
-                    <label className='block' htmlFor="name">Name</label>
-                    <input className='outline-none p-2 rounded-sm w-[100%]' placeholder='Hot Pink Chocolate' type="text" name="name" id="name"/>
-                </div>
-                <div>
-                    <label className='block' htmlFor="country">Email</label>
-                    <input className='outline-none p-2 rounded-sm w-[100%]' placeholder='Enter Country Name' type="email" name="country" id="name"/>
-                </div>
-                <div>
-                    <label htmlFor="gender">Gender</label>
-                    <input className='ml-8' type="radio" name="gender" value="Male" id="male"/> 
-                    <label className='ml-2' htmlFor='male'>Male</label>
-                    <input className='ml-8' type="radio" name="gender" value="Felame" id="female"/> 
-                    <label className='ml-2' htmlFor='female'>Female</label>
-                    <input className='ml-8' type="radio" name="gender" value="Other" id="other"/>
-                    <label className='ml-2' htmlFor='other'>Other</label>
-                </div>
-                <div>
-                    <label htmlFor="status">Status</label>
-                    <input className='ml-8' type="radio" name="status" value="Active" id="active"/>
-                    <label className='ml-2' htmlFor='active'>Active</label>
-                    <input className='ml-8' type="radio" name="status" value="Inactive" id="inactive"/>
-                    <label className='ml-2' htmlFor='inactive'>Inactive</label>
-                </div>
-                <div>
-                    <button className='bg-[#16DB93] w-[100%] rounded-sm p-2 text-white font-bold'>Add A New User</button>
-                </div>
-            </form>
+      <Header></Header>
+      <Link to="/">
+        <p>
+          <i class="fa-solid fa-arrow-left-long me-2 my-3"></i>
+          <span>All Users</span>
+        </p>
+      </Link>
+      <div className="bg-[#ece8e8] py-5 flex flex-col items-center">
+        <div className="text-center my-5">
+          <h3 className="text-2xl font-semibold mb-2">New User</h3>
+          <p className="text-[16px] text-[#615d5d]">
+            Use the below form to create a new user
+          </p>
         </div>
+        <form className="w-[60%] mx-atuo p-3 space-y-4">
+          <div>
+            <label className="block" htmlFor="name">
+              Name
+            </label>
+            <input
+              className="outline-none p-2 rounded-sm w-[100%]"
+              placeholder="Hot Pink Chocolate"
+              type="text"
+              name="name"
+              id="name"
+            />
+          </div>
+          <div>
+            <label className="block" htmlFor="country">
+              Email
+            </label>
+            <input
+              className="outline-none p-2 rounded-sm w-[100%]"
+              placeholder="Enter Country Name"
+              type="email"
+              name="country"
+              id="name"
+            />
+          </div>
+          <div>
+            <label htmlFor="gender">Gender</label>
+            <input
+              className="ml-8"
+              type="radio"
+              name="gender"
+              value="Male"
+              id="male"
+            />
+            <label className="ml-2" htmlFor="male">
+              Male
+            </label>
+            <input
+              className="ml-8"
+              type="radio"
+              name="gender"
+              value="Felame"
+              id="female"
+            />
+            <label className="ml-2" htmlFor="female">
+              Female
+            </label>
+            <input
+              className="ml-8"
+              type="radio"
+              name="gender"
+              value="Other"
+              id="other"
+            />
+            <label className="ml-2" htmlFor="other">
+              Other
+            </label>
+          </div>
+          <div>
+            <label htmlFor="status">Status</label>
+            <input
+              className="ml-8"
+              type="radio"
+              name="status"
+              value="Active"
+              id="active"
+            />
+            <label className="ml-2" htmlFor="active">
+              Active
+            </label>
+            <input
+              className="ml-8"
+              type="radio"
+              name="status"
+              value="Inactive"
+              id="inactive"
+            />
+            <label className="ml-2" htmlFor="inactive">
+              Inactive
+            </label>
+          </div>
+          <div>
+            <button className="bg-[#16DB93] w-[100%] rounded-sm p-2 text-white font-bold">
+              Add A New User
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddUser
+export default AddUser;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Header from "../components/Header";
 import { Link, useNavigate } from "react-router-dom";
 const Home = () => {
-    const navigate = useNavigate()
-    const handleNavigation = ()=>navigate("/addUser")
+  const navigate = useNavigate();
+  const handleNavigation = () => navigate("/addUser");
   return (
     <div>
       <Header></Header>
       <div className="mb-8">
-        <button onClick={handleNavigation} className="border-solid border-[1px] border-gray-400 p-2 rounded-lg">
+        <button
+          onClick={handleNavigation}
+          className="border-solid border-[1px] border-gray-400 p-2 rounded-lg"
+        >
           <i class="fa-solid fa-plus"></i>
           <span className="mx-2">New User</span>
           <i class="fa-regular fa-user"></i>
@@ -16,42 +19,42 @@ const Home = () => {
       </div>
       <table className="w-[100%] text-center">
         <thead className="bg-[#16db9334]">
-            <tr>
-                <th className="p-3">ID</th>
-                <th className="p-3">Name</th>
-                <th className="p-3">Email</th>
-                <th className="p-3">Gender</th>
-                <th className="p-3">Status</th>
-                <th className="p-3">Action</th>
-            </tr>
+          <tr>
+            <th className="p-3">ID</th>
+            <th className="p-3">Name</th>
+            <th className="p-3">Email</th>
+            <th className="p-3">Gender</th>
+            <th className="p-3">Status</th>
+            <th className="p-3">Action</th>
+          </tr>
         </thead>
         <tbody>
-            <tr>
-                <td className="px-10 py-5">1</td>
-                <td className="px-10 py-5">Rafiuzzaman</td>
-                <td className="px-10 py-5">rafiuzzama75@gmail.com</td>
-                <td className="px-10 py-5">Male</td>
-                <td className="px-10 py-5">Active</td>
-                <td className="px-10 py-5 space-x-5">
-                <Link to="/editUser">
+          <tr>
+            <td className="px-10 py-5">1</td>
+            <td className="px-10 py-5">Rafiuzzaman</td>
+            <td className="px-10 py-5">rafiuzzama75@gmail.com</td>
+            <td className="px-10 py-5">Male</td>
+            <td className="px-10 py-5">Active</td>
+            <td className="px-10 py-5 space-x-5">
+              <Link to="/editUser">
                 <i class="fa-solid fa-pen bg-[#d6cfcf] p-2 rounded-sm text-[#774320] cursor-pointer"></i>
-                </Link>
-                <i class="fa-solid fa-xmark bg-[#d6cfcf] p-2 rounded-sm text-[#774320] cursor-pointer"></i>
-                </td>
-            </tr>
-            <tr>
-                <td className="px-10 py-5">2</td>
-                <td className="px-10 py-5">Rafiuzzaman</td>
-                <td className="px-10 py-5">rafiuzzama75@gmail.com</td>
-                <td className="px-10 py-5">Male</td>
-                <td className="px-10 py-5">Inactive</td>
-                <td className="px-10 py-5 space-x-5">
-                <Link to="/editUser">
+              </Link>
+              <i class="fa-solid fa-xmark bg-[#d6cfcf] p-2 rounded-sm text-[#774320] cursor-pointer"></i>
+            </td>
+          </tr>
+          <tr>
+            <td className="px-10 py-5">2</td>
+            <td className="px-10 py-5">Rafiuzzaman</td>
+            <td className="px-10 py-5">rafiuzzama75@gmail.com</td>
+            <td className="px-10 py-5">Male</td>
+            <td className="px-10 py-5">Inactive</td>
+            <td className="px-10 py-5 space-x-5">
+              <Link to="/editUser">
                 <i class="fa-solid fa-pen bg-[#d6cfcf] p-2 rounded-sm text-[#774320] cursor-pointer"></i>
-                </Link>
-                <i class="fa-solid fa-xmark bg-[#d6cfcf] p-2 rounded-sm text-[#774320] cursor-pointer"></i>
-                </td>
-            </tr>
+              </Link>
+              <i class="fa-solid fa-xmark bg-[#d6cfcf] p-2 rounded-sm text-[#774320] cursor-pointer"></i>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
